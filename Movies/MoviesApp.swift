9280c55aa487cb3bd050f80d2dc3bbb3110610fe
Hellen Ferrari <hellenc.ferrari@gmail.com>
2024-01-23 12:59:29 -0800

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright (c) Hellen Ferrari. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
