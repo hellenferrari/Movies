26b1fa0412339308441b09cb7a23cb9ae3bd270b
Hellen Ferrari <hellenc.ferrari@gmail.com>
2024-01-23 17:21:34 -0800

    Specify all the session, view model and base URL that will be used

--- Movies/MoviesApp.swift
+++ Movies/MoviesApp.swift
@@ -8,7 +8,12 @@ import SwiftUI
 struct MoviesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let session = URLSession.shared
+            let httpClient = URLSessionHTTPClient(session: session)
+            let url = "https://www.omdbapi.com/"
+            let moviesService = MoviesService(httpClient: httpClient, baseUrl: url)
+            let viewModel = MoviesViewModel(service: moviesService)
+            MoviesListView(viewModel: viewModel)
         }
     }
 }
