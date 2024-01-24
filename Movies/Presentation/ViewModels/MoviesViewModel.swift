b64aafc0aea9f5126c81ebec4f31d2b3ba491c6c
Hellen Ferrari <hellenc.ferrari@gmail.com>
2024-01-23 19:36:06 -0800

    Add logic to searchText

--- Movies/Presentation/ViewModels/MoviesViewModel.swift
+++ Movies/Presentation/ViewModels/MoviesViewModel.swift
@@ -3,25 +3,40 @@
 //
 
 import Foundation
+import Combine
 
 class MoviesViewModel: ObservableObject {
     let service: MoviesService
+    @Published var searchText = ""
+    @Published var movies = [Movie]()
+    @Published var loadingState = LoadingState.loading
+    private var cancellables: Set<AnyCancellable> = []
     
     init(service: MoviesService) {
         self.service = service
+        
+        $searchText
+            .removeDuplicates()
+            .debounce(for: .seconds(2), scheduler: RunLoop.main)
+            .sink { [weak self] text in
+                guard let self = self else { return }
+                Task {
+                    await self.fetchMovies(with: text)
+                }
+            }
+            .store(in: &cancellables)
     }
-    
-    @Published var movies = [Movie]()
-    @Published var loadingState = LoadingState.loading
     
     enum LoadingState {
         case loading, loaded, empty, failed
     }
     
     @MainActor
-    func fetchMovies(content: String) async {
+    func fetchMovies(with content: String) async {
         do {
-            movies = try await service.getMovies(content)
+            let originalString = content
+            let stringWithPercent = originalString.replacingOccurrences(of: " ", with: "%20")
+            movies = try await service.getMovies(stringWithPercent)
         } catch {
             loadingState = .failed
         }
